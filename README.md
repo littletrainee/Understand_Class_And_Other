fa26ed0948fc009f887657c287bfef389beb5eb0
Logan <arechery.a1111@gmail.com>
2022-04-18 15:51:43 +0800

    Create Understand_Class_And_Other

---
+++ README.md
@@ -0,0 +1,1 @@
+Simple UnderStand Class And Other
---
+++ WpfApp1/user2/User2.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp1.user2 {
+  /// <summary>
+  /// User2.xaml 的互動邏輯
+  /// </summary>
+  public partial class User2 : UserControl {
+    public User2() {
+      InitializeComponent();
+    }
+
+
+    private void Button(object sender, RoutedEventArgs e) {
+      TotalLabel.Content = TotalLabel.Content.ToString() + Rt(ChineseLabel.Content.ToString(),
+        MathLabel.Content.ToString(), SciencesLabel.Content.ToString(),
+        SocialLabel.Content.ToString());
+    }
+
+    private int Rt(string chinesetext, string mathtext, string sciencestext,
+      string socialtext) {
+      return Convert.ToInt32(chinesetext.Substring(3)) +
+        Convert.ToInt32(mathtext.Substring(3)) +
+        Convert.ToInt32(sciencestext.Substring(3)) +
+        Convert.ToInt32(socialtext.Substring(3));
+    }
+  }
+}
